0baecec624e48a619b4b174cbc6ca54d9ecbedcc
Richard <Richard@home>
2016-06-08 04:06:47 +0200

    All endpoints has been updated to work with the new model and entity. We return a conflict error in case we want to create a Product or ProductOption already in the system We integrate the productId (without changing the API endpoint) on CreateOption and UpdateOption as the model represented on README does not mention ProductId has being part of the ProductOption JSON representation A few XML comments has been added

--- refactor-me/Controllers/ProductsController.cs
+++ refactor-me/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
 using System.Net;
 using System.Web.Http;
 using refactor_me.Models;
@@ -8,108 +11,254 @@ namespace refactor_me.Controllers
     [RoutePrefix("products")]
     public class ProductsController : ApiController
     {
+        private readonly DatabaseEntities _db;
+
+        public ProductsController(DatabaseEntities context)
+        {
+            _db = context;
+        }
+
+
+        public ProductsController()
+        {
+            _db = new DatabaseEntities(); 
+        }
+
+
+        /// <summary>
+        /// Gets all products
+        /// </summary>
         [Route]
         [HttpGet]
         public Products GetAll()
         {
-            return new Products();
+            return new Products(_db.Products);
         }
 
+        /// <summary>
+        /// Finds all products matching the specified name.
+        /// </summary>
         [Route]
         [HttpGet]
         public Products SearchByName(string name)
         {
-            return new Products(name);
+            return new Products(_db.Products.Where(p => p.Name.ToLower().Contains(name)));
         }
 
+        /// <summary>
+        /// Gets the product that matches the specified ID
+        /// </summary>
+        /// <param name="id">GUID of the product</param>
+        /// <returns></returns>
         [Route("{id}")]
         [HttpGet]
         public Product GetProduct(Guid id)
         {
-            var product = new Product(id);
-            if (product.IsNew)
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
                 throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
 
             return product;
         }
 
+        /// <summary>
+        /// Creates a new product
+        /// </summary>
         [Route]
         [HttpPost]
         public void Create(Product product)
         {
-            product.Save();
+            _db.Products.Add(product);
+            try
+            {
+                _db.SaveChanges();
+            }
+            // If we cannot store a product, it is likely it is already here
+            catch (DbUpdateException)
+            {
+                if (_db.Products.Count(p => p.Id == product.Id) > 0)
+                {
+                    throw new HttpResponseException(HttpStatusCode.Conflict);
+                }
+                throw;
+            }
         }
 
+        /// <summary>
+        /// Updates a product
+        /// </summary>
+        /// <param name="id">Id of the product to update</param>
+        /// <param name="product">Product with updated field</param>
         [Route("{id}")]
         [HttpPut]
         public void Update(Guid id, Product product)
         {
-            var orig = new Product(id)
+            if (id != product.Id)
             {
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                DeliveryPrice = product.DeliveryPrice
-            };
+                // We do not let the client changing the product ID
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+                        
+            _db.Entry(product).State = EntityState.Modified;
 
-            if (!orig.IsNew)
-                orig.Save();
+            try
+            {
+                _db.SaveChanges();
+            }
+             catch (DbUpdateException)
+            {
+                // Are we trying to update a non existing product?
+                if (_db.Products.Count(p => p.Id == id) == 0)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+                throw;
+            }
         }
 
+        /// <summary>
+        /// Deletes a product and its options.
+        /// </summary>
+        /// <param name="id">ID of the product to delete</param>
         [Route("{id}")]
         [HttpDelete]
         public void Delete(Guid id)
         {
-            var product = new Product(id);
-            product.Delete();
+            var product = _db.Products.Find(id);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            
+            _db.ProductOptions.RemoveRange(product.ProductOptions);
+            _db.Products.Remove(product);
+            _db.SaveChanges();
         }
 
+        /// <summary>
+        /// Finds all options for a specified product
+        /// </summary>
+        /// <param name="productId">ID of the product</param>
+        /// <returns></returns>
         [Route("{productId}/options")]
         [HttpGet]
         public ProductOptions GetOptions(Guid productId)
         {
-            return new ProductOptions(productId);
+            var product = _db.Products.Find(productId);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            // For that specific usage we need to tell entity to load ProductOptions related to our Product
+            _db.Entry(product).Collection(p => p.ProductOptions).Load();
+
+            return new ProductOptions(product.ProductOptions);
         }
 
+        /// <summary>
+        /// Finds the specified product option for the specified product
+        /// </summary>
+        /// <param name="productId">ID of the product</param>
+        /// <param name="id">ID of the product option</param>
+        /// <returns></returns>
         [Route("{productId}/options/{id}")]
         [HttpGet]
         public ProductOption GetOption(Guid productId, Guid id)
         {
-            var option = new ProductOption(id);
-            if (option.IsNew)
+            var productOption = _db.ProductOptions.Find(id);
+            if (productOption == null)
+            {
                 throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
 
-            return option;
+            return productOption;
         }
 
+        /// <summary>
+        /// Adds a new product option to the specified product
+        /// </summary>       
         [Route("{productId}/options")]
         [HttpPost]
         public void CreateOption(Guid productId, ProductOption option)
         {
+            var product = _db.Products.Find(productId);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            // as the ProductOption representation is not expected to contain the productId it is safer to add it here
             option.ProductId = productId;
-            option.Save();
+
+            product.ProductOptions.Add(option);
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (_db.ProductOptions.Count(po => po.Id == option.Id) > 0)
+                {
+                    throw new HttpResponseException(HttpStatusCode.Conflict);
+                }
+                throw;
+            }
         }
 
+        /// <summary>
+        /// Updates the specified product option.
+        /// </summary>
+        /// <param name="productId">ID of the product</param>
+        /// <param name="id">ID of the product option</param>
+        /// <param name="option">Updated product option</param>
         [Route("{productId}/options/{id}")]
         [HttpPut]
-        public void UpdateOption(Guid id, ProductOption option)
+        public void UpdateOption(Guid productId, Guid id, ProductOption option)
         {
-            var orig = new ProductOption(id)
+            if (id != option.Id)
             {
-                Name = option.Name,
-                Description = option.Description
-            };
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
 
-            if (!orig.IsNew)
-                orig.Save();
+            // as the ProductOption representation is not expected to contain the productId it is safer to add it here
+            option.ProductId = productId;
+
+            _db.Entry(option).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                // are we sure the option already exist?
+                if (_db.ProductOptions.Count(po => po.Id == option.Id) == 0)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+                throw;
+            }
         }
 
+        /// <summary>
+        /// deletes the specified product option
+        /// </summary>
         [Route("{productId}/options/{id}")]
         [HttpDelete]
         public void DeleteOption(Guid id)
         {
-            var opt = new ProductOption(id);
-            opt.Delete();
+            var productOption = _db.ProductOptions.Find(id);
+            if (productOption == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _db.ProductOptions.Remove(productOption);
+            _db.SaveChanges();
         }
     }
 }
