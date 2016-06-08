6c50b08243fecaf9eb89a9ee8360e6c804cf008d
Richard <Richard@home>
2016-06-08 03:38:32 +0200

    Generate an entity data model

---
+++ refactor-me/Models/DbContext.Context.cs
@@ -0,0 +1,20 @@
+﻿
+namespace refactor_me.Models
+{
+    using System;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public partial class DatabaseEntities : DbContext
+    {
+        public DatabaseEntities()
+            : base("name=DatabaseEntities")
+        {
+        }
+    
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductOption> ProductOptions { get; set; }
+        
+    }
+}
